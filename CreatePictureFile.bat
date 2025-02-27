9ad98078a8c5183c229efd26ec23f3f057c9ab74
raindazo <121225363+raindazo@users.noreply.github.com>
2025-02-27 18:59:58 +0900

    Update CreatePictureFile.bat

--- CreatePictureFile.bat
+++ CreatePictureFile.bat
@@ -1,23 +1,9 @@
-rem 以下の2行はいじらないでください。
+rem 最後以外の項目はいじらないでください。
 @echo off
 chcp 65001
 
-rem イコール(=)の右に指定されている値を入力してください
-
-rem VRCの写真が格納されているパス(例：C:\Users\user\画像\VRChat (yyyy-MMが保存されているファイルです))
-set vrcPictureFilePath=
-
-rem 撮影日分けされたファイルを保存するパス(例：C:\Users\output\画像\VRChat)
-set createPictureFilePath=
-
-rem 写真の名前をログに記録するか(記録する：True , 記録しない：False)
-set showDeletedPictureName=True
-
-
-rem 以下の2行はいじらないでください。
 cd ./CreateTodaysFile
-java -jar CreateTodaysFile.jar %vrcPictureFilePath% %createPictureFilePath% %showDeletedPictureName%
-
+java -jar CreateTodaysFile.jar %1
 
 rem 処理終了後自動的にコマンドプロンプトを閉じたい場合は以下を削除してください
 pause:
