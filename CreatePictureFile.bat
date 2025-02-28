d7b5b473222674f984803036697497308f27a6bb
raindazo <121225363+raindazo@users.noreply.github.com>
2025-02-28 15:27:21 +0900

    Update CreatePictureFile.bat

--- CreatePictureFile.bat
+++ CreatePictureFile.bat
@@ -1,9 +1,10 @@
 rem 最後以外の項目はいじらないでください。
 @echo off
-chcp 65001
+chcp 65001 >nul
 
-cd ./CreateTodaysFile
-java -jar CreateTodaysFile.jar %1
+setlocal enabledelayedexpansion
+cd /d "%~dp0CreateTodaysFile"
+java -Dfile.encoding=UTF-8 -jar CreateTodaysFile.jar %1
 
 rem 処理終了後自動的にコマンドプロンプトを閉じたい場合は以下を削除してください
 pause:
